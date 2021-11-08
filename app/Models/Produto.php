59410c5732df5b20fa1b4110c7bc2742be432e79
Wagner Pitelli <wpitelli@gmail.com>
2021-11-07 21:31:53 -0300

    Update Produto.php

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+    protected $fillable = ['nome', 'custo', 'preco', 'quantidade'];
 }
