bfee2a93c390130eb921143ae20a5012bb82df9a
Wagner Pitelli <wpitelli@gmail.com>
2021-11-08 10:43:28 -0300

    Criado em ProdutosContreoller.php a função create
    Criado em ProdutosContreoller.php a função create

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class ProdutosController extends Controller
 {
+    public function create() {
+    
+        return view('produtos create');
+
+    }
+       
     //
 }
